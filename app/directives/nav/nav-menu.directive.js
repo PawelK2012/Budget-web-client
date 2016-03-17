05b338c2ca1158bdae7ed6f2d2e407a19c426a4b
Pawel Kaim <pawelkai@ie.ibm.com>
2016-03-17 20:01:58 +0000

    Passing currenUsert to the nav-menu directive. Implemented global logOut()

--- app/directives/nav/nav-menu.directive.js
+++ app/directives/nav/nav-menu.directive.js
@@ -6,46 +6,21 @@
     .directive('navMenu', function() {
         return {
             restrict: 'E',
-            //replace: 'true',
             templateUrl: 'directives/nav/nav-menu.partial.html',
             controller: NavMenuController,
             controllerAs: 'vm',
             bindToController: true,
-            scope: {}
+            scope: {
+                currentUser: '=currentUser'
+            }
         };
     });
 
-    function NavMenuController() {
+    function NavMenuController(authenticationService) {
         var vm = this;
-        vm.consoleLog = function(){
-            console.log("nav menu")
+        vm.logOut = function() {
+           authenticationService.getLogout();
         }
-
     }
 
 })();
-
-
-// (function() {
-//     'use strict';
-//     angular.module('myApp.navmenu')
-//         .directive('navMenu', NavMenu);
-
-//     function NavMenu() {
-//         var directive = {
-//             restrict: 'E',
-//             //replace: 'true',
-//             templateUrl: 'directives/nav/nav-menu.partial.html',
-//             controller: NavMenuController,
-//             controllerAs: 'vm',
-//             bindToController: true,
-//             scope: {}
-//         };
-//         return directive;
-//     }
-
-//     function NavMenuController() {
-//         var vm = this;
-
-//     }
-// })();
