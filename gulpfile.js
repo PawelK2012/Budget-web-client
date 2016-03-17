c6b49170143ed40113f9bf4aae87cbefd2c50e70
Pawel Kaim <pawelkai@ie.ibm.com>
2016-03-17 23:03:25 +0000

    Created scss modules for different directives.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
     gulp.src('app/sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('./app/css/'))
 });
 
@@ -13,3 +13,4 @@ gulp.task('styles', function() {
 gulp.task('default',function() {
     gulp.watch('app/sass/**/*.scss',['styles']);
 });
+
