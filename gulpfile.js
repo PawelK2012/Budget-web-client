22b8e8cc83dc2f6435c46e0c2f86b62b92d7c396
Pawel Kaim <pawelkai@ie.ibm.com>
2016-03-19 19:14:23 +0000

    Concat all SASS files into one  styles.min.css file.

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,15 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
 
 gulp.task('styles', function() {
-    gulp.src('app/sass/**/*.scss')
+  return gulp.src('app/sass/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(concat('style.min.css'))
+        .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest('./app/css/'))
 });
 
