4ff3f964aab7994448d7dbfba0165b4f7412060a
Pawel Kaim <pawelkai@ie.ibm.com>
2016-10-21 20:17:23 +0100

    Protractor E2E test for login view
    E2E test for login view

---
+++ conf.js
@@ -0,0 +1,20 @@
+exports.config = {
+    framework: 'jasmine',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['todo-spec.js'],
+    capabilities: {
+        browserName: 'chrome'
+    }
+};
+
+
+// exports.config = {
+//   framework: 'jasmine',
+//   seleniumAddress: 'http://localhost:4444/wd/hub',
+//   specs: ['todo-spec.js'],
+//   multiCapabilities: [{
+//     //browserName: 'firefox'
+//   }, {
+//     browserName: 'chrome'
+//   }]
+// }
